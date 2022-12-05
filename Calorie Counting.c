9106b0cd2ae747f3ae19a91575a6fb1ea0b70d2e
Oddstap <41316908+oddstap@users.noreply.github.com>
2022-12-05 07:40:03 -0600

    Update Calorie Counting.c

--- Calorie Counting.c
+++ Calorie Counting.c
@@ -8,7 +8,7 @@ int main(int argc, char **argv)
     char buffer[MAX_LENGTH];
     FILE *fp = fopen(argv[1], "r");
     int array[1000] = {0};
-    int sum = 0, i = 0, anwser_1 = 0, anwser_2 = 0, anwser_3 = 0;
+    int sum = 0, i = 0, answer_1 = 0, answer_2 = 0, answer_3 = 0;
 
     if(argv[1] <= 0){
         printf("No file argument\n");
@@ -30,22 +30,22 @@ int main(int argc, char **argv)
     }
 
     for(int j = 0; j < 1000; ++j){
-        if(anwser_2 > array[j] && anwser_3 < array[j]){
-            anwser_3 = array[j];
+        if(answer_2 > array[j] && answer_3 < array[j]){
+            answer_3 = array[j];
         }
-        else if(anwser_2 < array[j] && anwser_2 < anwser_1){
-            anwser_2 = array[j];
+        else if(answer_2 < array[j] && answer_2 < answer_1){
+            answer_2 = array[j];
         }
-        else if(anwser_1 < array[j]){
-            anwser_1 = array[j];
+        else if(answer_1 < array[j]){
+            answer_1 = array[j];
         }
     }
 
-    printf("anwser 1 is %d\n", anwser_1);
-    printf("answer 2 is %d\n", anwser_2);
-    printf("answer 3 is %d\n", anwser_3);
+    printf("answer 1 is %d\n", answer_1);
+    printf("answer 2 is %d\n", answer_2);
+    printf("answer 3 is %d\n", answer_3);
 
-    printf("Total sum of all three is %d\n", anwser_1 + anwser_2 + anwser_3);
+    printf("Total sum of all three is %d\n", answer_1 + answer_2 + answer_3);
    
     fclose(fp);
     return 0;
