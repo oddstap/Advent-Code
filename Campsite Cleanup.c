9eff12a6ba2fc7dc39015cc438313ee1ee2a5277
Oddstap <41316908+oddstap@users.noreply.github.com>
2022-12-05 03:23:22 -0600

    Update Campsite Cleanup.c

--- Campsite Cleanup.c
+++ Campsite Cleanup.c
@@ -7,20 +7,26 @@ int main()
     char buffer[MAX_LENGTH];
     char *filename = "input.txt";
     FILE *fp = fopen(filename, "r");
-    int answer = 0;
+    int answer_1 = 0, answer_2 = 0;
 
     while(fgets(buffer, MAX_LENGTH, fp)){
 
         int a, b, c, d;
 
         sscanf(buffer, "%d-%d, %d-%d", &a, &b, &c, &d );
-
+        
         if (a >= c && b <= d || c >= a && d <= b){
-            answer += 1;
+            answer_1 += 1;
         }
+        
+        if (a <= d && b >= c){
+            answer_2 += 1;
+        }
+        
     }
 
-    printf("%d\n", answer);
+    printf("Anwser for part 1: %d\n", answer_1);
+    printf("Anwser for part 2: %d\n", answer_2);
     fclose(fp);
 
     return 0;
